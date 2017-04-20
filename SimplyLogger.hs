0564bfa7afb1cd6e056839948c9d4fe45cbcebee
ISafd <idolarod1@gmail.com>
2017-04-20 10:33:37 +0300

    Обновлён логгер
    Упрощёно переключение между акторами. Теперь это выглядит не настолько плохо)

--- SimplyLogger.hs
+++ SimplyLogger.hs
@@ -49,64 +49,49 @@ putInCPocket (MemConstr qm cm qp _) cBit =  MemConstr qm cm qp (CPoc cBit)
 popFromCPocket :: Memory -> CBit
 popFromCPocket (MemConstr qm cm qp (CPoc cBit)) = cBit
 
+actorName :: Bool -> String
+actorName nameFlag = if nameFlag then "Alice" else "Bob"
+
 -- интерприторот
 commandlog :: Memory -> Program () -> Program () -> Bool -> IO ()
-commandlog memory alice bob flag = 
-    let progM = if flag == True then alice else bob in
-        let progS = if flag == True then bob else alice in
-            let name = if flag == True then "Alice" else "Bob" in
-                case progM of
-                Free (QInit bit next) -> do
-                    putStrLn $ "This is " ++ name
-                    let (c, mem) = addIncQ memory in do
-                        putStrLn $ "Init new qubit with number " ++ show c ++ ".\n"
-                        if flag == True 
-                            then commandlog mem (next (QBitConstr c)) progS flag
-                            else commandlog mem progS (next (QBitConstr c)) flag
-                Free (CInit bit next) -> do
-                    putStrLn $ "This is " ++ name
-                    let (c, mem) = addIncC memory in do
-                        putStrLn $ "Init new classic bit with number" ++ show c ++ ".\n"
-                        if flag == True 
-                            then commandlog mem (next (CBitConstr c)) progS flag
-                            else commandlog mem progS (next (CBitConstr c)) flag
-                Free (Measure (QBitConstr qbitN) next) -> do
-                    putStrLn $ "This is " ++ name ++ "\n" ++ "Qubit with number " ++ show qbitN ++ " measured.\n"
-                    if flag == True 
-                        then commandlog memory (next (QBitConstr qbitN)) progS flag
-                        else commandlog memory progS (next (QBitConstr qbitN)) flag  
-                Free (QGate (QBitConstr qbitN) next) -> do
-                    putStrLn $ "This is " ++ name ++ "\n" ++ "Qubit with number " ++ show qbitN ++ " went through the Gate!\n"
-                    if flag == True 
-                        then commandlog memory (next (QBitConstr qbitN)) progS flag
-                        else commandlog memory progS (next (QBitConstr qbitN)) flag  
-                Free (SendQMessage (QBitConstr qbitN) next) -> do
-                    putStrLn $ "This is " ++ name ++ "\n" ++ "Qubit with number " ++ show qbitN ++ " sent to partner.\n"
-                    if flag == True 
-                        then commandlog mem (next) progS (not flag)
-                        else commandlog mem progS (next) (not flag)  where
-                            mem = putInQPocket memory (QBitConstr qbitN) 
-                Free (RecieveQMessage next) -> do
-                    putStrLn $ "This is " ++ name ++ "\n" ++ "Qubit with number " ++ show qbitN ++ " recieved from partner.\n"
-                    if flag == True 
-                        then commandlog memory (next (QBitConstr qbitN)) progS flag
-                        else commandlog memory progS (next (QBitConstr qbitN)) flag  where
-                            (QBitConstr qbitN) = popFromQPocket memory
-                Free (SendCMessage (CBitConstr qbitN) next) -> do
-                    putStrLn $ "This is " ++ name ++ "\n" ++ "Classic bit with number " ++ show qbitN ++ " sent to partner.\n"
-                    if flag == True 
-                        then commandlog mem (next) progS (not flag)
-                        else commandlog mem progS (next) (not flag)  where
-                            mem = putInCPocket memory (CBitConstr qbitN) 
-                Free (RecieveCMessage next) -> do
-                    putStrLn $ "This is " ++ name ++ "\n" ++ "Classic bit with number " ++ show cbitN ++ " recieved from partner.\n"
-                    if flag == True 
-                        then commandlog memory (next (CBitConstr cbitN)) progS flag
-                        else commandlog memory progS (next (CBitConstr cbitN)) flag  where
-                            (CBitConstr cbitN) = popFromCPocket memory
-                Pure r -> do
-                    putStrLn $ name ++ "'s all!"
-                    return r
+commandlog memory progM progS nameFlag = 
+    let name = actorName nameFlag in
+        case progM of
+        Free (QInit bit next) -> do
+            putStrLn $ "This is " ++ name
+            let (c, mem) = addIncQ memory in do
+                putStrLn $ "Init new qubit with number " ++ show c ++ ".\n"
+                commandlog mem (next (QBitConstr c)) progS nameFlag
+        Free (CInit bit next) -> do
+            putStrLn $ "This is " ++ name
+            let (c, mem) = addIncC memory in do
+                putStrLn $ "Init new classic bit with number" ++ show c ++ ".\n"
+                commandlog mem (next (CBitConstr c)) progS nameFlag
+        Free (Measure (QBitConstr qbitN) next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubit with number " ++ show qbitN ++ " measured.\n"
+            commandlog memory (next (QBitConstr qbitN)) progS nameFlag
+        Free (QGate (QBitConstr qbitN) next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubit with number " ++ show qbitN ++ " went through the Gate!\n"
+            commandlog memory (next (QBitConstr qbitN)) progS nameFlag
+        Free (SendQMessage (QBitConstr qbitN) next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubit with number " ++ show qbitN ++ " sent to partner.\n"
+            commandlog mem progS (next) (not nameFlag) where
+                mem = putInQPocket memory (QBitConstr qbitN) 
+        Free (RecieveQMessage next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubit with number " ++ show qbitN ++ " recieved from partner.\n"
+            commandlog memory (next (QBitConstr qbitN)) progS nameFlag where
+                (QBitConstr qbitN) = popFromQPocket memory
+        Free (SendCMessage (CBitConstr qbitN) next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Classic bit with number " ++ show qbitN ++ " sent to partner.\n"
+            commandlog mem progS (next) (not nameFlag) where
+                mem = putInCPocket memory (CBitConstr qbitN) 
+        Free (RecieveCMessage next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Classic bit with number " ++ show cbitN ++ " recieved from partner.\n"
+            commandlog memory (next (CBitConstr cbitN)) progS nameFlag where
+                (CBitConstr cbitN) = popFromCPocket memory
+        Pure r -> do
+            putStrLn $ name ++ "'s all!"
+            return r
 
 emptyMemory = MemConstr (QMemConstr []) (CMemConstr []) (QPoc (QBitConstr 0)) (CPoc (CBitConstr 0))
 
