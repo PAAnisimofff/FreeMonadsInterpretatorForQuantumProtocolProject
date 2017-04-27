d41c2a341655e91d772ad7faf4ef1e46d141a180
ISafd <idolarod1@gmail.com>
2017-04-27 04:43:19 +0300

    Обьединение скриптов
    Функция mix обьединяет два скрипта в один, переключаясь при отправке сообщений.
    Добавлен тестовый ad-hoc интерпретарор работающий с одним скриптом

--- SimplyLogger.hs
+++ SimplyLogger.hs
@@ -1,7 +1,12 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+
 module SimplyLogger where
 
 import System.IO
 import Control.Monad.Free
+import Control.Comonad.Cofree
 import Lang
 
 data QMemory = QMemConstr [Int]
@@ -119,3 +124,78 @@ emptyMemory = MemConstr (QMemConstr []) (CMemConstr []) (QPoc []) (CPoc [])
 
 simplyLog :: Program () -> Program () -> IO ()
 simplyLog alice bob = commandlog emptyMemory alice bob True
+
+
+
+
+
+-- обьединяет две программы в одну с переключаясь при отправке
+mix :: Program r -> Program r -> Program r
+mix progS progM = 
+    case progM of
+        Free (SendQMessage qbits next) -> Free (SendQMessage qbits ((mix next) progS))
+        Free (SendCMessage cbits next) -> Free (SendCMessage cbits ((mix next) progS))
+        Free a -> Free (fmap (mix progS) a)
+        x -> x
+
+-- эти тут для тестов
+commandlogB :: Memory -> Program () -> Bool -> IO ()
+commandlogB memory progM nameFlag = 
+    let name = actorName nameFlag in
+        case progM of
+        Free (QInit bitList next) -> do
+            putStrLn $ "This is " ++ name
+            let (qbitNs, newMemory) = addQBits memory (length bitList) in do
+                putStrLn $ "Init new qubits with numbers " ++ show qbitNs ++ ".\n"
+                commandlogB newMemory (next (map QBitConstr qbitNs)) nameFlag
+        Free (CInit bitList next) -> do
+            putStrLn $ "This is " ++ name
+            let (cbitNs, newMemory) = addCBits memory (length bitList) in do
+                putStrLn $ "Init new classic bits with numbers " ++ show cbitNs ++ ".\n"
+                commandlogB newMemory (next (map CBitConstr cbitNs)) nameFlag
+        Free (Measure qbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbits ++ " measured to classic bits with numbers" ++ show cbitNs ++ "\n"
+            commandlogB newMemory (next (map CBitConstr cbitNs)) nameFlag where
+                (cbitNs, newMemory) = addCBits memory (length qbits)
+        Free (QGate qbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbits ++ " went through the Abstract Unnamed Gate!\n"
+            commandlogB memory (next qbits) nameFlag
+        Free (SendQMessage qbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbits ++ " sent to partner.\n"
+            commandlogB newMemory (next) (not nameFlag) where
+                newMemory = putInQPocket memory qbits
+        Free (RecieveQMessage next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbits ++ " recieved from partner.\n"
+            commandlogB memory (next qbits) nameFlag where
+                qbits = popFromQPocket memory
+        Free (SendCMessage cbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Classic bits with numbers " ++ showCBits cbits ++ " sent to partner.\n"
+            commandlogB newMemory (next) (not nameFlag) where
+                newMemory = putInCPocket memory cbits
+        Free (RecieveCMessage next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Classic bits with numbers " ++ showCBits cbits ++ " recieved from partner.\n"
+            commandlogB memory (next cbits) nameFlag where
+                cbits = popFromCPocket memory
+        Free (Hadamard qbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbits ++ " went through the Hadamard Gate.\n"
+            commandlogB memory (next qbits) nameFlag
+        Free (PauliX qbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbits ++ " went through the PauliX Gate.\n"
+            commandlogB memory (next qbits) nameFlag
+        Free (PauliY qbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbits ++ " went through the PauliY Gate.\n"
+            commandlogB memory (next qbits) nameFlag
+        Free (PauliZ qbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbits ++ " went through the PauliZ Gate.\n"
+            commandlogB memory (next qbits) nameFlag
+        Free (CNot qbitsC qbits next) -> do
+            putStrLn $ "This is " ++ name ++ "\n" ++ "Qubits with numbers " ++ showQBits qbitsC ++ " and " ++ showQBits qbits ++ " went through the CNot Gate.\n"
+            commandlogB memory (next (qbitsC ++ qbits)) nameFlag
+        Pure r -> do
+            putStrLn $ name ++ "'s all!"
+            return r
+
+simplyLogB :: Program () -> IO ()
+simplyLogB both = commandlogB emptyMemory both True
+
+
